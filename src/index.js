98f70495f5d4af17e1aeee1e98a660bcc5869c16
AppleJaxxZ <smackanalex@gmail.com>
2021-10-14 20:25:21 -0400

    Chat app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("Connection", () => {
+  console.log("new websocket connection");
+});
+
+server.listen(port, () => {
+  console.log(`Express server started at ${port}`);
+});
